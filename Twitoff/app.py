346303387e4917c6d13f57dbc07e94a9ecedd0ab
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-12 21:31:35 -0500

    Assignment

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -1,6 +1,6 @@
 from decouple import config
 from flask import Flask, render_template, request
-from .models import DB, User
+from .models import DB, User, Tweet
 
 #now we make a app factory
 
@@ -19,9 +19,14 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
+    @app.route('/user/<name>')
+    def user(name):
+        tweets = Tweet.query.filter(User.name == name).tweets
+        return render_template('tweets.html', title='tweets', tweets=tweets)
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
         DB.create_all()
-        return render_template('base.html', title='Home', users=users)
+        return render_template('base.html', title='Reset', users=[])
     return app
--- Twitoff/twitter.py
+++ Twitoff/twitter.py
@@ -13,3 +13,17 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
 #add more functions here
+
+def get_user(username):
+    twitter_user = TWITTER.get_user(username)
+    db_user = User(id=twitter_user.id, name=username)
+    DB.session.add(db_user)
+    tweets = twitter_user.timeline(count=200, exlude_replies=True,
+                            include_rts=False, tweet_mode='extended')
+    for tweet in tweets:
+        embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+        db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+        embedding=embedding)
+        db_user.tweets.append(db_tweet)
+        DB.session.add(db_tweet)
+    DB.session.commit()
