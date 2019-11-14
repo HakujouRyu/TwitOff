2d286f3ed3afe528a73b1ce4604fe18fa0bf8f71
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-14 13:47:03 -0500

    Change timeout

--- Twitoff/models.py
+++ Twitoff/models.py
@@ -1,28 +1,27 @@
-"""These are my database models"""
+
+"""SQLALchemy models for Twitoff"""
 
 from flask_sqlalchemy import SQLAlchemy
 
-#import database. capital for global scope
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    """Twitter users that we analyze"""
+    """Twitter users that we pull and analyze"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+
     newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
-
 class Tweet(DB.Model):
-    """The user's tweets from twitter"""
+    """Tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(300))
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable = False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
-    embedding = DB.Column(DB.PickleType, nullable=False)
-
     def __repr__(self):
-        return '<Tweet {}>'.format(self.text)
+	       return '<Tweet {}>'.format(self.text)
