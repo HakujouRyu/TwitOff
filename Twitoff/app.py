b1d6e3d620c143498693f12dac6883eae545568f
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-14 12:29:24 -0500

    Initial commit

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -1,7 +1,13 @@
-from decouple import config
-from flask import Flask, render_template, request
-from .models import DB, User, Tweet
+"""Build app factory and routes and configuration"""
 
+from decouple import config
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
+from .predict import predict_user
+
+load_dotenv()
 #now we make a app factory
 
 def create_app():
@@ -19,10 +25,35 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
 
-    @app.route('/user/<name>')
-    def user(name):
-        tweets = Tweet.query.filter(User.name == name).tweets
-        return render_template('tweets.html', title='tweets', tweets=tweets)
+    #adding in a new route to add users or get users
+    @app.route('/user', methods=['POST']) #uses form
+    @app.route('/user/<name>', methods=['GET']) #needs parameter
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+
+ #adding in a route for predictions
+    @app.route('/compare', methods=['POST'])
+    def compare(message=''):
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     @app.route('/reset')
     def reset():
